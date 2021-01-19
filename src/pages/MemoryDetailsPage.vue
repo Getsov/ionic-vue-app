d65fb95a25b40c76a41b22b0b9e42acf3a63d256
Getsov <valsvetget@abv.bg>
2021-01-19 16:33:07 +0200

    adding Add Memory option, breaking the pages on Components instead and saving and displaying the added memory

--- src/pages/MemoryDetailsPage.vue
+++ src/pages/MemoryDetailsPage.vue
@@ -4,12 +4,22 @@
     page-default-back-link="/memories"
   >
     <h2 v-if="!loadedMemory">Could not find a memory for the given id.</h2>
-    <h2 v-else>Loaded it</h2>
+    <!-- <h2 v-else>Loaded it</h2> -->
+    <memory-overview
+      v-else
+      :title="loadedMemory.title"
+      :description="loadedMemory.description"
+      :image="loadedMemory.image"
+    ></memory-overview>
   </base-layout>
 </template>
 
 <script>
+import MemoryOverview from "../components/memories/MemoryOverview.vue";
 export default {
+  components: {
+    MemoryOverview,
+  },
   data() {
     return {
       memoryId: this.$route.params.id,
@@ -20,6 +30,7 @@ export default {
       return this.$store.getters.memory(this.memoryId);
     },
   },
+
   //   watch: {
   //     $route(currentRoute) {
   //       this.memoryId = currentRoute.params.id;
